7ef254ffd6b21146d3d695e5d1e6d0b98d0d7ffa
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2021-03-05 10:43:40 +0700

    commit at home

--- src/component/collection/Collection.jsx
+++ src/component/collection/Collection.jsx
@@ -1,55 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "../collection/Collection.css";
-import Collection1 from "../../img/forweb.png";
-import Collection2 from "../../img/forweb2.jpg";
+import Collection1 from "../../img/forweb2.jpg";
+import Collection2 from "../../img/forweb3.jpg";
 import hoverEffect from 'hover-effect';
 import Distor from '../../img/10.jpg';
 
 function Collection() {
-    
-    Array.from(document.querySelectorAll('.collection__img-border')).forEach((e)=>{
-        const imgs = Array.from(e.querySelectorAll('img'));
-       const hover =  new hoverEffect({
-            parent: e,
-            intensity: 0.3,
-            image1: imgs[0].getAttribute('src'),
-            image2: imgs[1].getAttribute('src'),
-            displacementImage: Distor,
-        }); 
-});
-
+    useEffect(() => {
+        Array.from(document.querySelectorAll('.collection__img-border')).forEach((e) => {
+            const imgs = Array.from(e.querySelectorAll('img'));
+            new hoverEffect({
+                parent: e,
+                intensity: 0.3,
+                image1: imgs[0].getAttribute('src'),
+                image2: imgs[1].getAttribute('src'),
+                displacementImage: Distor,
+            });
+        });
+    }, [])
     return (
         <div className="collection">
             <div className="collection--wrapper">
-            <div className="collection__content">
-                <div className="col__content--head">
-                    <p>MEN'S COLLECTION</p>
-                </div>
+                <div className="collection__content">
+                    <div className="col__content--head">
+                        <p>MEN'S COLLECTION</p>
+                    </div>
                     <div className="col__content--middle">
                         <p>The Art - Classic Fusion</p>
                     </div>
-                <div className="col__content--h1">
-                    <p>TOURBILLON POWER</p>
-                </div>
+                    <div className="col__content--h1">
+                        <p>TOURBILLON POWER</p>
+                    </div>
                     <div className="col__content--describe">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                            Inventore et labore consequuntur 
-                            iste necessitatibus tempore ut veniam,
-                            consectetur ex non nam quod eveniet laborum modi laboriosam 
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                        Inventore et labore consequuntur
+                        iste necessitatibus tempore ut veniam,
+                        consectetur ex non nam quod eveniet laborum modi laboriosam
                             fugiat adipisci dolorum. Sed.</p>
                     </div>
-            <div className="col__content--view">
-                <a href="/">view collection</a>
-            </div>
-            </div>
-            <div className="collection__img">
-                <div className="col__img__border-absolute"></div>
-                <div className="collection__img-border">
-                        <img className="collection__img--single" src={Collection1} alt="collapse"/>
-                        <img className="collection__img--single" src={Collection2} alt="collapse2"/>
+                    <div className="col__content--view">
+                        <a href="/">view collection</a>
+                    </div>
+                </div>
+                <div className="collection__img">
+                    <div className="col__img__border-absolute"></div>
+                    <div className="collection__img-border">
+                        <img className="collection__img--single" src={Collection1} alt="collapse" />
+                        <img className="collection__img--single" src={Collection2} alt="collapse2" />
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
